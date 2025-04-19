474297dac0f7e69016bfe4409f47ac8e6fce03e7
rishi-kayal-bio <kayalrishi8@gmail.com>
2025-04-19 14:48:57 +0530

    Update Chainsaman.py

--- Chainsaman.py
+++ Chainsaman.py
@@ -18,8 +18,8 @@ def create_connection():
     
 
 
-DB_PATH = "/mnt/data/chainsaw.db"  # This is the persistent directory on StackHost
-os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+#DB_PATH = "/mnt/data/chainsaw.db"  # This is the persistent directory on StackHost
+#os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
 
 conn = sqlite3.connect(DB_PATH)
 def create_table():
