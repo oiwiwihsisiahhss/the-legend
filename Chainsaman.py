f11cba9db719dd4088a0b4e36003082820695a2e
rishi-kayal-bio <kayalrishi8@gmail.com>
2025-06-16 09:32:12 +0530

    Update Chainsaman.py

--- Chainsaman.py
+++ Chainsaman.py
@@ -1951,7 +1951,7 @@ def explore(message):
     # Anti-spam: 15 second cooldown
     if user_id in last_explore_time:
         diff = (now - last_explore_time[user_id]).total_seconds()
-        if diff < 0.5:
+        if diff < 5:
             bot.send_message(message, "🛑 Hey kid, stop spamming! Try again in a few seconds.")
             return
 
